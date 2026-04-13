d5c52be84664df36241dd5ebbd8362333a987665
ankitkushwaha05 <ankitkushwaha841204@gmail.com>
2026-04-13 17:40:04 +0530

    elem

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,20 +3,34 @@ import React from 'react'
 function btnClicked(){
   console.log("i am clicked");
 }
-
+ function inputChanging(elem) {
+        console.log(val);
+        
+      }
 export const App = () => {
   return (
     <div>
 
-      <button onDoubleClick={btnClicked}>Clisk here</button>
+      {/* <button onDoubleClick={btnClicked}>Clisk here</button>
       <button onClick={function(){
         console.log("hello anknit");
       }}>ankit</button>
-      <h1>this is button</h1>
-      <input onChange={function(){
-        console.log("this is input click")
-      }} type="text" placeholder='Enter name' />
+      <h1>this is button</h1> */}
+
+     
+      <input onChange={function(elem){
+        console.log(elem.target.value)
+      }} 
+      type="text"
+      placeholder='Enter name' />
+
+
     </div>
   )
 }
 export default App;
+
+// chal coffee piwayenge 
+// gama me light ko ni aandi
+// bistara makhmal ke nhi mite
+// chal chandigarh ghuma lau
