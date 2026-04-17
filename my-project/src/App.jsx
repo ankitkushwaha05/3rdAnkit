193fdcdd150c38d5a153489a4efda3827ef6418c
ankitkushwaha05 <ankitkushwaha841204@gmail.com>
2026-04-17 18:11:59 +0530

    usestate

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,55 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// function btnClicked(){
-//   console.log("i am clicked");
-// }
-//  function inputChanging(elem) {
-//         console.log(val);
-        
-//       }
 
-const pageScrolling = =>{
-  console.log("i am entring form");
-}
-export const App = () => {
+
+const App = () => {
+  
+  
+  const [num, setNum] = useState(0)
+
+  function increaseNum(){
+    setNum(num+1);
+
+  }
+  function decreaseNum(){
+    setNum(num-1);
+  }
+  function jump5num(){
+    setNum(num+5)
+  }
+
   return (
-    <div onScroll={}>
-
-      {/* <button onDoubleClick={btnClicked}>Clisk here</button>
-      <button onClick={function(){
-        console.log("hello anknit");
-      }}>ankit</button>
-      <h1>this is button</h1> */}
-
-     
-      {/* <input onChange={function(elem){
-        console.log(elem.target.value)
-      }} 
-      type="text"
-      placeholder='Enter name' /> */}
-      {/* <div onMouseMove={() =>{
-        console.log('elem');
-      }} className='box'></div> */}
-
-      <div className="page1">
-
-      </div>
-      <div className="page2"></div>
-      <div className="page3"></div>
-
-    </div>
+    <>
+    <h1>{num}</h1>
+    <button onClick={increaseNum}>increase</button>
+    <button onClick={decreaseNum}>decrease</button>
+    <button onClick={jump5num}>Incress by 5</button>
+    </>
   )
 }
-<form action="">
-  <input type="text" placeholder='Enter your name'/>
-  <input type="text" placeholder='Enter your Email Id' />
-  <input type="number" placeholder='Emter your number'/>
-  <input type="text" placeholder='Enter your Perfect Message '/>
-  <input type="text" />
-</form>
-export default App;
 
-// chal coffee piwayenge 
-// gama me light ko ni aandi
-// bistara makhmal ke nhi mite
-// chal chandigarh ghuma lau
+export default App; 
